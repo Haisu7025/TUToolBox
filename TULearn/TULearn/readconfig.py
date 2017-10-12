90a3f550d0616746fe1a29463dd1a590e00de486
haisu7025 <haisu.7025@gmail.com>
2017-10-12 15:15:04 +0800

    add unfinished configparser

--- TULearn/TULearn/readconfig.py
+++ TULearn/TULearn/readconfig.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import configparser
 import os
 
@@ -5,7 +7,19 @@ import os
 def getConfig(section="", key=""):
     config = configparser.ConfigParser()
     path = os.path.split(os.path.realpath(__file__))[0] + '/user.conf'
-    print path
+    config.read(path)
+    print os.path.exists(path)
+    return config.get(section, key)
 
 
-getConfig()
+def setConfig(section="", key="", value=""):
+    config = configparser.ConfigParser()
+    path = os.path.split(os.path.realpath(__file__))[0] + '/user.conf'
+    config.read(path)
+    config.add_section("user")
+    config.set(section, key, value)
+    config.write(open("user.conf", "w"))
+
+
+setConfig("user", "name", "test")
+print getConfig("user", "name")
